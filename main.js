1cfc0b606b260e4317c60431bb26b0e09ac470f0
jeemee-simform <jeemee.sakariya@simformsolutions.com>
2026-02-24 20:46:58 +0530

    validation par completed

--- main.js
+++ main.js
@@ -13,16 +13,19 @@ btnDiv.addEventListener("click", (e) => {
       display.innerText = "";
       return;
     case "=":
-      valid() ? output() : (display.innerHTML = "Error");
+      valid()
+        ? output()
+        : display.innerText == ""
+          ? (display.innerText = "")
+          : (display.innerText = "Error");
       return;
   }
-  display.innerText += value ? value : id;
+  if (value !== "trigonometry") display.innerText += value ? value : id;
 });
 
 function valid() {
   const str = display.innerText;
   let bracketCount = 0;
-  console.log(str);
 
   // bracket validater
   for (const char of str) {
@@ -32,7 +35,93 @@ function valid() {
       bracketCount--;
     }
   }
-  return 0;
+  if (bracketCount) return false;
+
+  // check char alowed or not
+  const allowedPattern =
+    /^(?:3\.14159|2\.71828|sin\(|cos\(|cosec\(|sec\(|cot\(|ten\(|log\(|ln\(|\^\-1|\^2|[0-9+\-*/%^√!().])+$/;
+  if (!allowedPattern.test(str)) return false;
+
+  // check desimal validation
+  const invalidDesimal = /\d*\.\d*\./;
+  if (invalidDesimal.test(str)) return false;
+
+  // no consecutiv binary operator allowed
+  const consecutivOperator = /[+\-*/%^]{2,}/;
+  if (consecutivOperator.test(str)) return false;
+
+  // invalid start and end
+  const invalidStart = /^[*/%^]/;
+  const invalidEnd = /[+\-*/%^√(]$/;
+  if (invalidStart.test(str) || invalidEnd.test(str)) return false;
+
+  // check postfix operqator
+  const invalidPostfixOperator = /(^|[+\-*/%^√(])(\!|\^2|\^\-1)/;
+  // factorial must not follow decimal
+  const invalidFactorialDecimal = /\d+\.\d+!/;
+  if (invalidPostfixOperator.test(str) || invalidFactorialDecimal.test(str))
+    return false;
+
+  //in valid prefix operator
+  const invalidPrefixOperator =
+    /(\d|\)|!|\^2|\^\-1)(√|sin\(|cos\(|cosec\(|sec\(|cot\(|ten\(|log\(|ln\()/;
+  if (invalidPrefixOperator.test(str)) return false;
+
+  // invalid function usege
+  const invalidFunctionUsage = /(sin|cos|cosec|sec|cot|ten|log|ln)(?!\()/;
+  if (invalidFunctionUsage.test(str)) return false;
+
+  // invalid parenthesis ending: only !, numbers, or functions allowed before ')'
+  const invalidParenEnd = /\([^\)]*([+\-*/%^√])\)/;
+  if (invalidParenEnd.test(str)) return false;
+
+  return true;
 }
 
-function output() {}
+function funEvaluate(fun, value) {
+  switch (fun) {
+    case "sin":
+      return Math.sin((value * Math.PI) / 180);
+    case "cos":
+      return Math.cos((value * Math.PI) / 180);
+    case "tan":
+      return Math.tan((value * Math.PI) / 180);
+    case "cosec":
+      return 1 / Math.sin((value * Math.PI) / 180);
+    case "sec":
+      return 1 / Math.cos((value * Math.PI) / 180);
+    case "cot":
+      return 1 / Math.tan((value * Math.PI) / 180);
+    case "log":
+      return Math.log10(value);
+    case "ln":
+      return Math.log(value);
+  }
+}
+
+function replaceFunctionWithValue(str) {
+  // const functions = ["sin", "cos", "tan", "cosec", "sec", "cot", "log", "ln"];
+  const findFuncRegex =
+    /(sin|cos|tan|cosec|sec|cot|log|ln)\(([+-]?\d+(\.\d+)?)\)/g;
+
+  const result = str.replace(findFuncRegex, (match, fun, value) => {
+    if ((fun === "log" || fun === "ln") && parseInt(value) < 0) return 0;
+    return funEvaluate(fun, value);
+  });
+  return result;
+}
+
+function output() {
+  const str = display.innerText;
+
+  const result = replaceFunctionWithValue(str);
+  console.log(typeof result, result);
+  if (result === "0") {
+    display.innerText = "Error";
+    return;
+  }
+
+  const ans = eval(result);
+
+  display.innerText = ans;
+}
